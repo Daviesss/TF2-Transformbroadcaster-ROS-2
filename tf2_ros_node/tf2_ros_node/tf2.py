f3ed5b0db14853bf105833586105f42079ba7214
Daviesss <ogunsdavis53@gmail.com>
2023-12-10 09:48:17 +0100

    TF2 ROS 2 broadcaster node

--- tf2_ros_node/tf2_ros_node/tf2.py
+++ tf2_ros_node/tf2_ros_node/tf2.py
@@ -3,28 +3,26 @@ import rclpy
 from rclpy.node import Node 
 from geometry_msgs.msg import TransformStamped
 from tf2_ros import TransformBroadcaster 
-# import time
+
 
 class Transform_list(Node):
     def __init__(self):
         super().__init__("trasnform_listner_node")
         self.Transform_broadcast = TransformStamped()
         self.broadcaster = TransformBroadcaster(self)
-        # self.publisher = self.create_publisher(TransformStamped,'/tf_frame',10)
         self.timer = 0.05 
         self.timeer = self.create_timer(self.timer,self.control_callback)
 
     # control method
     def control_callback(self):
-        # transform = self.tf_buffer.lookup_transform_core('table1','map',rclpy.time.Time())
-        # self.get_logger().info('The transforms has been received %r' %transform)
         self.Transform_broadcast.header.stamp = self.get_clock().now().to_msg()
-        self.Transform_broadcast.header.frame_id = 'map'
-        self.Transform_broadcast.child_frame_id = 'table1'
+        self.Transform_broadcast.header.frame_id = 'map' # origin frame/fixed_frame name 'map'
+        self.Transform_broadcast.child_frame_id = 'table1' # child_frame/frame which is attaced to the origin frame/world_frame
         self.Transform_broadcast.transform.translation.x = 1.5
         self.Transform_broadcast.transform.translation.y = -0.5
         self.Transform_broadcast.transform.translation.z = 0.92
-
+        
+        # rotation of the frame 
         self.Transform_broadcast.transform.rotation.x = 0.0
         self.Transform_broadcast.transform.rotation.y = 0.0
         self.Transform_broadcast.transform.rotation.z = 0.0
